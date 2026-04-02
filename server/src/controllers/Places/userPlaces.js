1f732233f0035787a83ac6b8dac58f10b20ac994
bnloading <nurbakhitjan5@gmail.com>
2026-04-02 17:42:31 +0500

    Update Render URL and fix CORS

--- server/src/controllers/Places/userPlaces.js
+++ server/src/controllers/Places/userPlaces.js
@@ -1,22 +1,26 @@
 const Place = require("../../models/Place");
 const jwt = require("jsonwebtoken");
 
-const userPlaces = async (req, res) => {
+const userPlaces = async (req, res, next) => {
+  try {
     const { token } = req.cookies;
 
     if (!token) {
-        throw new Error("Authorization token not found.");
+      return res.status(401).json({ error: "Authorization token not found." });
     }
 
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.id;
 
     if (!userId) {
-        throw new Error("User ID not found in token.");
+      return res.status(401).json({ error: "User ID not found in token." });
     }
 
     const places = await Place.find({ owner: userId });
     res.json(places);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = userPlaces;
