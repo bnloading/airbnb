c8649a77ed39a0477a652afeaf387cc00122624e
bnloading <nurbakhitjan5@gmail.com>
2026-04-02 16:52:12 +0500

    thirdвывы

--- server/index.js
+++ server/index.js
@@ -79,4 +79,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.listen(5002, () => console.log("Server is listening on port 5002..."));
+const PORT = process.env.PORT || 5002;
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
